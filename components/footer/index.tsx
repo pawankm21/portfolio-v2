d23c3263c6b7efe41675079c609e1bcb8c09503a
Pawan Kumar Mishra <63885768+pawankm21@users.noreply.github.com>
2022-06-30 00:04:51 +0530

    fix icons

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,12 +1,21 @@
 import { motion } from "framer-motion";
 import { delayVariant } from "../../utils/utils";
+import GithubIcon from "../icons/github-icon";
+import LinkedinIcon from "../icons/linkedin-icon";
+import MailIcon from "../icons/mail-icon";
+import TwitterIcon from "../icons/twitter-icon";
 export default function Footer() {
   return (
     <motion.div
       variants={delayVariant}
       animate="animate"
       initial="initial"
-      className="bg-zinc-800 p-4 text-neutral-400"
-    ></motion.div>
+      className="bg-zinc-800 h-20 flex place-items-center justify-center text-neutral-400 gap-4"
+    >
+      <TwitterIcon className="w-6 h-6" />
+      <GithubIcon className="w-6 h-6"/>
+      <LinkedinIcon className="w-6 h-6" />
+      <MailIcon className="w-6 h-6"/>
+    </motion.div>
   );
 }
