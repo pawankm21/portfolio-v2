d26c54891e68bbb41a398b0a696dd4edd9d86ad2
Pawan Kumar Mishra <pawan994567@gmail.com>
2023-03-24 15:04:53 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,10 +80,7 @@ const Home: NextPage = ({ blogs, projects, skills }: any) => {
               <motion.div className="lg:w-1/2 lg:block hidden">
                 <div className=" flex justify-center align-top -z-10 h-full relative">
                   <CircleAnimation />
-                  <div className="relative w-full flex justify-items-start">
-                    <Image src="/assets/me.png" width="250"
-                      objectFit="cover" height="250" alt="Me" />
-                  </div>
+                    <img src="/assets/me.png" width="250" className=" object-cover self-center align-self-center justify-self-center" alt="Me" />
                 </div>
               </motion.div>
             </motion.div>
